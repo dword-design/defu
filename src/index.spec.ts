3b2091b8fb918f85b7c8fa9d7d13f06c4e6a2d0d
Sebastian Landwehr <info@sebastianlandwehr.com>
2025-07-13 12:17:15 +0200

    fix: createDefu

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,11 +1,39 @@
 import { expect, test } from '@playwright/test';
 
-import self from '.';
+import self, { createDefu } from '.';
 
-test('array', () =>
-  expect(self({ plugins: ['local'] }, { plugins: ['inherited'] })).toEqual({
-    plugins: ['inherited', 'local'],
-  }));
+test.describe('default', () => {
+  test('array', () =>
+    expect(self({ plugins: ['local'] }, { plugins: ['inherited'] })).toEqual({
+      plugins: ['inherited', 'local'],
+    }));
 
-test('object', () =>
-  expect(self({ a: 2, b: 1 }, { a: 1, c: 1 })).toEqual({ a: 2, b: 1, c: 1 }));
+  test('object', () =>
+    expect(self({ a: 2, b: 1 }, { a: 1, c: 1 })).toEqual({ a: 2, b: 1, c: 1 }));
+});
+
+test.describe('createDefu', () => {
+  test('merger', () => {
+    const defu = createDefu((obj, key, value) => {
+      obj[key] = value;
+      return true;
+    });
+
+    expect(defu({ a: [2] }, { a: [1] })).toEqual({ a: [2] });
+  });
+
+  test('array', () => {
+    const defu = createDefu((obj, key, value) => {
+      if (key === 'foo') {
+        obj[key] = value;
+        return true;
+      }
+
+      return false;
+    });
+
+    expect(
+      defu({ a: ['local'], foo: [1] }, { a: ['inherited'], foo: [2] }),
+    ).toEqual({ a: ['inherited', 'local'], foo: [1] });
+  });
+});
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,21 @@
-import { createDefu } from 'defu';
+import { createDefu as createDefuFromDefu } from 'defu';
 
-export default createDefu((obj, key, value): boolean => {
+const arrayMerger = (obj, key, value): boolean => {
   if (Array.isArray(obj[key]) && Array.isArray(value)) {
     obj[key].push(...(value as unknown[])); // TODO: Type cast shouldn't be necessary
     return true;
   }
 
   return false;
-});
+};
+
+export default createDefuFromDefu(arrayMerger);
+
+export const createDefu = merger =>
+  createDefuFromDefu((obj, key, value) => {
+    if (merger(obj, key, value)) {
+      return true;
+    }
+
+    return arrayMerger(obj, key, value);
+  });
